cd0eb811d98a5fcb36a14fcb107e32f1bf63d3c2
ParkSB <parkgds@gmail.com>
2019-07-21 16:11:33 +0900

    feat: Add naver english dictionary

---
+++ src/Scraper.ts
@@ -0,0 +1,20 @@
+
+import Content from './Content';
+
+class Scraper {
+  static baseUrl: string;
+  static content: Content = {
+    title: '',
+    part: '',
+    pronounce: '',
+    description: '',
+    example: '',
+    exampleMeaning: '',
+  };
+
+  static load(word: string) {
+    throw new Error('It should be implemented');
+  }
+}
+
+export default Scraper;
