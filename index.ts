de50540bd667d3d2148431ab558784439d51c90f
parksb <parkgds@gmail.com>
2022-12-13 00:40:29 +0900

    check trigger key on mouseup

--- index.ts
+++ index.ts
@@ -7,12 +7,26 @@ document.addEventListener('mouseup', (e) => {
     Tooltip.hide();
   }
 
-  if (!Tooltip.getIsOpen() && e.altKey) {
-    const word = document.getSelection()?.toString().trim();
-    if (word != null && word.length > 0 && word.length < 40) {
-      if (Dictionary.load(word) !== Languages.Undefined) {
-        Tooltip.show();
+  const showTooltip = (withAlt: boolean) => {
+    const keyHolded = withAlt ? e.altKey : true;
+    if (!Tooltip.getIsOpen() && keyHolded) {
+      const word = document.getSelection()?.toString().trim();
+      if (word != null && word.length > 0 && word.length < 40) {
+        if (Dictionary.load(word) !== Languages.Undefined) {
+          Tooltip.show();
+        }
       }
     }
-  }
+  };
+
+  const onError = () => {
+    showTooltip(true);
+  };
+
+  const onTriggerKeyGot = (item: { triggerKey: 'with-alt' | 'without-any-key' }) => {
+    showTooltip(item.triggerKey === 'with-alt');
+  };
+
+  const triggerKey = browser.storage.sync.get('triggerKey');
+  triggerKey.then(onTriggerKeyGot, onError);
 });
